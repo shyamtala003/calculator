f35016cf45c9afed40b841ae22d2a5e7020962bc
shyamtala003 <shyamtala003@gmail.com>
2022-09-26 20:59:53 +0530

    V.1.0

--- main.js
+++ main.js
@@ -1,2 +1,32 @@
 let resultDiv = document.getElementById('result');
 let equationDiv = document.getElementById('equation');
+
+let input = "";
+let result = "";
+
+let getInput = (data) => {
+    input += data.value;
+    resultDiv.innerHTML = input;
+}
+
+let removeLastNumber = () => {
+    input = input.slice(0, -1);
+    resultDiv.innerHTML = input;
+    equationDiv.innerHTML="";   
+}
+
+let reset = () => {
+    input = "";
+    resultDiv.innerHTML = input;
+    equationDiv.innerHTML=""
+}
+
+let evalute = () => {
+    if(input.length<=0)
+    {
+        equationDiv.innerHTML="";
+    }else{
+        equationDiv.innerHTML = input+"=";
+        resultDiv.innerHTML = eval(input);
+    }
+}
