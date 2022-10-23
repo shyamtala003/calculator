6a4a844674e9f82f1202968ca7e264af4915ce22
shyamtala003 <shyamtala003@gmail.com>
2022-10-23 16:49:21 +0530

    add subtraction button and chage the multiplication and division button sign

--- main.js
+++ main.js
@@ -12,38 +12,37 @@ let getInput = (data) => {
 let removeLastNumber = () => {
     input = input.slice(0, -1);
     resultDiv.innerHTML = input;
-    equationDiv.innerHTML="";   
+    equationDiv.innerHTML = "";
 }
 
 let reset = () => {
     input = "";
     resultDiv.innerHTML = input;
-    equationDiv.innerHTML=""
+    equationDiv.innerHTML = ""
 }
 
 let evalute = () => {
-    if(input.length<=0)
-    {
-        equationDiv.innerHTML="";
-    }else{
-        equationDiv.innerHTML = input+"=";
+    if (input.length <= 0) {
+        equationDiv.innerHTML = "";
+    } else {
+        input = input.replaceAll("×", "*");
+        input = input.replaceAll("÷", "/");
+        equationDiv.innerHTML = input + "=";
         resultDiv.innerHTML = eval(input);
     }
 }
 
 let themeToggler = (el) => {
-    
-    if(document.body.classList.contains("light"))
-    {
+
+    if (document.body.classList.contains("light")) {
         document.body.classList.remove("light");
         document.body.classList.add("dark");
-        let ionIcon =el.firstElementChild;
-        ionIcon.setAttribute("name","sunny-outline");
-    }else
-    {
+        let ionIcon = el.firstElementChild;
+        ionIcon.setAttribute("name", "sunny-outline");
+    } else {
         document.body.classList.remove("dark");
         document.body.classList.add("light");
-        let ionIcon =el.firstElementChild;
-        ionIcon.setAttribute("name","moon-outline");
+        let ionIcon = el.firstElementChild;
+        ionIcon.setAttribute("name", "moon-outline");
     }
 }
